e4fd7bf7227e9460cc3bb83653da2ab5b853bd28
timpec <timpe96@hotmail.com>
2023-09-12 11:33:47 +0300

    Take number of items, price of items and calculate

--- task.py
+++ task.py
@@ -1,2 +1,8 @@
-number = input("Enter")
-print(number)
+numberOfItems = input("Enter number of items: ")
+price = input("Enter price of item: ")
+
+print("Number of items: "+numberOfItems)
+print("Price of individual item: "+price)
+
+totalPrice = int(numberOfItems) * int(price)
+print(totalPrice)
